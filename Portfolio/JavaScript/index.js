7954d3b21afe0ee459c5acdae05551617dcc2ccd
Slou <gael.pilet1@gmail.com>
2024-10-14 10:48:36 +0200

    UP html-css index (not finished yet)

--- Portfolio/JavaScript/index.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
-
-menuToggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
